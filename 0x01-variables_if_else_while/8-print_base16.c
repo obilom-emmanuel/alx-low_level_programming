611e8d3c67de315d4c67a3d75dfb0a3f3ecbf5b7
obilom-emmanuel <uchechukwuemmanuel81@gmail.com>
2023-05-19 17:00:42 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,16 +10,15 @@
 int main(void)
 {
 	int n;
-	int l = 97;
+	int l;
 
-	for (n = 48; n <= 57; n++);
+	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
 	}
-	while (l <= 102)
+	for (l = 97; l <= 102; l++)
 	{
 		putchar(l);
-		l++;
 	}
 	putchar('\n');
 	return (0);
