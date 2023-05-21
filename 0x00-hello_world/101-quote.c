acd3b4947b8eda12b92371ea83cc745b1d6abd9f
obilom-emmanuel <uchechukwuemmanuel81@gmail.com>
2023-05-21 13:39:54 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,8 @@
 
 int main(void)
 {
-	fputs("\n"and that piece of art is useful" - Dora Korpar,
-		       2015-10-19", stdout);
-	retoun(0);
+	write(2,
+			"and that piece of art is useful\" - Dora Korpar,
+		       2015-10-19\n", 59);
+	return (1);
 }
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,9 @@
  * Return: 0 (Success)
  */
 
-int main(void);
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 
 	return (0);
 }
