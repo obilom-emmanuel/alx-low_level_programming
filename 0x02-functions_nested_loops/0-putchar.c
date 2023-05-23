9b16eaa35716d1c48c42375b10be6e63202a4aeb
obilom-emmanuel <uchechukwuemmanuel81@gmail.com>
2023-05-23 13:22:44 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description - A program that prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	putchar('\n');
+	return (0);
+}
