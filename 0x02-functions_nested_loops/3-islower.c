a8f4ee0c1b1c0884f9ba6f73f345ec244ffd86bc
obilom-emmanuel <uchechukwuemmanuel81@gmail.com>
2023-05-24 16:37:27 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ * _islower - only lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	if (c = 97 && c >= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
