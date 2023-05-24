ed3e73de3b6296a4d25d20dda0f7b729357b3685
obilom-emmanuel <uchechukwuemmanuel81@gmail.com>
2023-05-24 07:00:42 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,6 +9,17 @@
 
 int main(void)
 {
-	putchar('\n');
+	char str[] = "_putchar";
+
+	for (int i = 0; i <= 12; i++)
+	{
+		if (str[i] == '\0')
+		{
+			putchar('\n');
+			break;
+		}
+		else
+			putchar(str[i]);
+	}
 	return (0);
 }
