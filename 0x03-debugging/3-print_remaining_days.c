fa83aac0b7b1c16f361e7a1ad57e836eec6898b6
obilom-emmanuel <uchechukwuemmanuel81@gmail.com>
2023-05-25 12:32:27 -0700

    0x03-debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,11 +10,11 @@
  * Return: void.
  */
 
-void print_remainin_days(int month, int day, int year)
+void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
-		if (month > 2 && day is >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
