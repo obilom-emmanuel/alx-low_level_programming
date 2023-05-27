4ff9b0636820c63eb36fdd78466981c015df06e3
obilom-emmanuel <uchechukwuemmanuel81@gmail.com>
2023-05-27 03:16:56 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,19 @@
 
 void print_line(int n)
 {
-	int i;
+	int i = 0;
 
-	if (n <= 0)
+	if (n > 0)
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+	else if (n <= 0)
 	{
 		_putchar('\n');
 	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		_putchar('_');
-	}
-	_putchar('\n');
 }
