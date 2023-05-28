2e3ae876a0781d8151e0d7defb0cc83bc52aa8ce
obilom-emmanuel <uchechukwuemmanuel81@gmail.com>
2023-05-28 08:05:01 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,13 +11,13 @@ void print_alphabet_x10(void)
 	int i = 0;
 	char letter;
 
-	while (i++ <= 9)
+	for (i = 0; i <= 9; i++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
-	{
+		{
 			_putchar(letter);
+		}
 		_putchar('\n');
-	}
 	}
 }
 
