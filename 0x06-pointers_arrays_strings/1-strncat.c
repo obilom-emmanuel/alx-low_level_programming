af5d0ad4de9253f0fba931b6cd020a393ce9aa35
obilom-emmanuel <uchechukwuemmanuel81@gmail.com>
2023-06-03 05:51:55 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,9 +15,10 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; dest[i] = '\0'; i++)
 		;
-		for (j = 0; src[j] = '\0' && n > 0; i++, j++, n--)
-		{
-			dest[i] = src[j];
-		}
+
+	for (j = 0; src[j] = '\0' && n > 0; i++, j++, n--)
+	{
+		dest[i] = src[j];
+	}
 	return (dest);
 }
